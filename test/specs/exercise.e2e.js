c6534dfadf7fab57b24709343212a3deac323303
Tve idealne github jmeno <hanka.peroutkova@centrum.cz>
2023-10-17 23:40:16 +0200

    Lekce 2

--- test/specs/exercise.e2e.js
+++ test/specs/exercise.e2e.js
@@ -3,24 +3,101 @@ import {username, password} from './fixtures.js'
 // import ApplicationsPage from '../pageobjects/applications.page'
 
 describe('Czechitas Login Page', async () => {
-
+//describe(nazev funkce, to co vykona)
     it('should open login page', async () => {
-
+        //tohle je anonymni lambda funkce
+        //await znamena, ze je asynchronni - kdyby tam await, tak by mi vyjelo jen PROMISE
         await browser.reloadSession(); //otevre se nove okno prohlizec
         //otevre stranku prihlaseni
         await browser.url('/prihlaseni'); //na tu base url doplni /prihlaseni'
 
         //await browser.saveScreenshot('login.png')
 
-        //velikost okna prohlizece
-        const windowSize = await browser.getWindowSize();
-        console.log('Velikost okna: ',windowSize);
-        //udelat screenshot a ulozit ho do slozky screenshot
-        await browser.saveScreenshot('./screenshots/login_page_firefox.png');
-       //Zapauzujte vykonávání testu a 5 vteřin
-        await browser.pause(5000); //tohle se mi otevre a zustane stat 5 vterin
-        //console.log('Vypis neco')
-        
+    //     //velikost okna prohlizece
+    //     const windowSize = await browser.getWindowSize();
+    //     console.log('Velikost okna: ',windowSize);
+    //     //udelat screenshot a ulozit ho do slozky screenshot
+    //     await browser.saveScreenshot('./screenshots/login_page_firefox.png');
+    //    //Zapauzujte vykonávání testu a 5 vteřin
+    //     await browser.pause(5000); //tohle se mi otevre a zustane stat 5 vterin
+    //     //console.log('Vypis neco')
+
+
+    // //cviceni1 - podle tagu
+    //a.
+     console.log('Tady je 1.a');
+      const element_form = await  $('form').getHTML();
+      console.log(element_form);
+    //b.
+      console.log('Tady je 1.b');
+      const element_input  = await  $('input').getHTML();
+      console.log(element_input);
+    //c.
+    console.log('Tady je 1.c');
+    const element_btn  = await  $('button').getHTML();
+    console.log(element_btn);
+
+    // //cviceni 2 - podle ID
+       console.log('Tady je 2.a');
+        const email =await $('#email').getHTML();
+       console.log(email);
+
+       console.log('Tady je 2.b');
+         const password =await $('#password ').getHTML();
+       console.log(password ); 
+  
+
+    //cviceni 3 podle třídy
+    console.log('Tady je 3.a - talčítko na odeslání formuláře pomocí třídy');
+        const btnOdesli =await $('.btn-primary ').getHTML();
+    console.log(btnOdesli ); 
+
+    console.log('Tady je 3.b.	políčko email podle atributu name');
+        const fieldEmail =await $('[name="email"]').getHTML();
+    console.log(fieldEmail);
+
+    //cviceni 4 podle atributu
+    console.log('Tady je 4.a - políčko password podle attributu type');
+        const fieldPassword =await $('[type="password"]').getHTML();
+    console.log(fieldPassword ); 
+
+    console.log('Tady je 4.b. - políčko podle atributu jehož honota obsahuje “ass”');
+        const atrAss =await $('[type*="ass"]').getHTML();
+    console.log(atrAss);
+
+    console.log('Tady je 4.c - políčko podle atributu jehož honota končí na na “word”');
+        const atrWord =await $('[type$="word"]').getHTML();
+    console.log(atrWord);
+
+    console.log('Tady je 4.d.	políčko podle atributu jehož honota začíná na “pass”');
+        const atrPass =await $('[type^="pass"]').getHTML();
+    console.log(atrPass);
+
+    //cviceni 5 kombinace
+    console.log('Tady je 5.a.	podle kombinovaného selektoru pro tag input a id email');
+        const tagAndid  = await  $('input#email').getHTML();
+    console.log(tagAndid);
+
+    console.log('Tady je 5.b.	podle kombinovaného selektoru pro tag input a atribut type s hodnotou password');
+        const tagAndtype = await  $('input[type="password"]').getHTML();
+    console.log(tagAndtype);
+
+    console.log('Tady je 5.c.	podle kombinovaného selektoru pro tag button a třídu btn-primary');
+        const tagAndclass = await  $('button.btn-primary').getHTML();
+    console.log(tagAndclass);
+
+    //cviceni 6 řetězení selektorů
+    console.log('Tady je 6.	vytvoř řetězení $ pro tag div > form > input[type$="word"]');
+        const retezeni = await  $('div').$('form').$('input[type$="word"]').getHTML();
+    console.log(retezeni);
+
+    //cviceni 7 7.	WDIO selektory
+    console.log('Tady je 7.	najdi element podle textu "Zapomněli jste své heslo?"');
+        let WDIO = await $('.btn=Zapomněli jste své heslo?')
+    await expect(WDIO).toHaveText('Zapomněli jste své heslo?')
+    console.log(WDIO);
+
+
     });
 
 });
--- test/specs/homework/homework.e2e.js
+++ test/specs/homework/homework.e2e.js
@@ -6,7 +6,31 @@ describe('Homework', async () => {
         await browser.reloadSession();
         await browser.url('/registrace'); 
         await browser.saveScreenshot('./screenshots/homework1.png');
-        
+
+        // ●	Políčko pro jméno a příjmení
+        console.log('Tady je Políčko pro jméno a příjmení');
+        const fldJmenoPrijmeni = await  $('[name="name"][type="text"]').getHTML();
+        console.log(fldJmenoPrijmeni);
+
+        // ●	Políčko pro email
+        console.log('Tady je ●	Políčko pro email');
+        const fldEmail = await  $('[name="email"][type="email"]').getHTML();
+        console.log(fldEmail);
+
+        // ●	Políčko pro zadání hesla
+        console.log('Tady je ●	Políčko pro zadání hesla');
+        const fldHeslo = await  $('[name="password"][type="password"]').getHTML();
+        console.log(fldHeslo);
+
+        // ●	Políčko pro kontrolu zadaného hesla
+        console.log('Tady je ●	Políčko pro kontrolu zadaného hesla');
+        const fldHesloPotvrzeni = await  $('#password-confirm').getHTML();
+        console.log(fldHesloPotvrzeni);
+
+        // ●	Tlačítko na registraci
+        console.log('Tady je ●	Tlačítko na registraci');
+        const btnRegistrace = await  $('[type="submit"].btn').getHTML();
+        console.log(btnRegistrace);
     });
 
 });
